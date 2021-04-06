b9bbc4adf40bf8d9999fd5d97788a5effb576008
marci546 <marton.veres0623@gmail.com>
2021-04-06 17:08:16 +0200

    mostmár működik is

--- app/src/main/java/com/example/szakdolgozat/MainActivity.java
+++ app/src/main/java/com/example/szakdolgozat/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.szakdolgozat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton img_bttn = findViewById(R.id.img_button);
+        img_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainscreen = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(mainscreen);
+                finish();
+            }
+        });
+
     }
+
 }
